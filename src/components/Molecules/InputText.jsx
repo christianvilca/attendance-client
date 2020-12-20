0f82d60ebb21acfed5d7c098ed89f997e4f13842
Christian Vilca <christian.vilcaq@gmail.com>
2020-12-19 19:15:07 -0500

    Update InputText component

--- src/components/Molecules/InputText.jsx
+++ src/components/Molecules/InputText.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const InputText = ({label: Label, ...props}) => {
+
+	const [value, setValue] = useState("");
+
 	return (
 		<div className="group">
-			<input className="input" type="text" required {...props} />
+			<input className="input" type="text" value={value} onChange={e => setValue(e.target.value)} required {...props} />
 			<span className="bar" />
 			<label className="label" >{Label}</label>
 		</div>
