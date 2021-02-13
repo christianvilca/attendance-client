9b5a6bf3c95ebe9b9c6ee3de30daa6dff592ef0e
Christian Vilca <christian.vilcaq@gmail.com>
2021-02-12 20:12:47 -0500

    perf(Apollo.jsx): Change connection and uri

--- src/apollo/Apollo.jsx
+++ src/apollo/Apollo.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 //import { ApolloProvider } from 'react-apollo'
 //import { ApolloProvider as ApolloHooksProvider} from '@apollo/react-hooks'
 //import ApolloClient, { InMemoryCache } from 'apollo-boost'
-import { ApolloClient, InMemoryCache, ApolloProvider as ApolloHooksProvider, HttpLink } from '@apollo/client'
+import { ApolloClient, InMemoryCache, ApolloProvider as ApolloHooksProvider, HttpLink, createHttpLink } from '@apollo/client'
 import App from './App.jsx'
 import AppRoutes from './AppRoutes.jsx'
 import { defaults, resolvers } from './resolvers'
+import { setContext } from 'apollo-link-context';
 
-const client = new ApolloClient({
+const client1 = new ApolloClient({
   cache: new InMemoryCache({
     addTypename: false
   }),
+  //cache: new InMemoryCache(),
   link: new HttpLink({
-    uri: 'http://localhost:4000/graphql',
+    uri: 'http://192.168.0.3:4000/graphql',
     //credentials: 'same-origin',
     headers: {
       authorization: localStorage.getItem('token')
@@ -38,6 +40,30 @@ const client = new ApolloClient({
     addTypename: false
   }), */
 })
+
+const uri = process.env.REACT_APP_URL_API;
+const httpLink = createHttpLink({ uri });
+const cache = new InMemoryCache({
+	addTypename: false
+});
+
+// return the headers to the context
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem('token') || ''
+    }
+  };
+});
+
+// create the Apollo client
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache,
+  resolvers: {},
+  connectToDevTools: true
+});
 
 const Apollo = () => (
   //<ApolloProvider client={client}>
