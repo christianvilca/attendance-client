944d8e03d4effe5b87c6a9a3db30f34889462d8c
Christian Vilca <christian.vilcaq@gmail.com>
2021-02-20 23:51:46 -0500

    refactor(ListCard.jsx): add useQuery

--- src/components/Organisms/ListCard.jsx
+++ src/components/Organisms/ListCard.jsx
@@ -1,30 +1,30 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Card from '../Molecules/Card.jsx';
-import Query from '../../apollo/Query.jsx';
+import Spinner from '../Atoms/Spinner.jsx';
+import { useQuery } from '@apollo/client';
 
-const ListCard = ({...props}) => {
+const ListCard = ({ query, ...props }) => {
+	const { loading, error, data } = useQuery(query, { ...props });
+	if (loading) return <Spinner classes="spinner-list" />;
+	if (error) return `Error! ${error.message}`;
+
 	return (
-		<div>
-			<Query classes="spinner-list" {...props} >
-				{({ getInstitutionList: data }) =>
-					data.length !== 0 ? (
-						data.map((item) => {
-							return (
-								<Card
-									key={item.id}
-									to={ "/institution/" + item.id}
-									image={item.image}
-									name={item.name}
-									location={item.location}
-									alias={item.alias}
-								/>
-							);
-						})
-					) : (
-						<span>Sin registros</span>
-					)}
-			</Query>
-		</div>
+		<Fragment>
+			{data.getInstitutionList.length !== 0 ? (
+				data.getInstitutionList.map((item) => (
+					<Card
+						key={item.id}
+						to={'/institution/' + item.id}
+						image={item.image}
+						name={item.name}
+						location={item.location}
+						alias={item.alias}
+					/>
+				))
+			) : (
+				<span>Sin registros</span>
+			)}
+		</Fragment>
 	);
 };
 
