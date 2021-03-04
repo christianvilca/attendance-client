8130bf6514fefc8ce76d4047b9fa97ad46a9ecf0
Christian Vilca <christian.vilcaq@gmail.com>
2021-03-04 14:35:29 -0500

    style: create _container-detail.scss

--- src/components/Pages/InstitutionQR.jsx
+++ src/components/Pages/InstitutionQR.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ContainerDetail from '../Templates/ContainerDetail.jsx';
 import InputText from '../Molecules/InputText.jsx';
-import QRCode  from 'qrcode.react'
+import QRCode from 'qrcode.react';
 
-const InstitutionQR = ({match, ...props}) => {
-
-	const hanldeChange =()=>{}
+const InstitutionQR = ({ match, ...props }) => {
+	const hanldeChange = () => {};
 
 	const propsHeader = {
 		title: 'Codigo de Institución',
@@ -14,26 +13,25 @@ const InstitutionQR = ({match, ...props}) => {
 		class1: 'button-header'
 	};
 
-	let value = match.params.id
+	let value = match.params.id;
 
 	const formatPhoneNumber = () => {
-		let match = value.match(/^(\w{6})(\w{6})(\w{6})(\w{6})$/)
-		console.log(match)
+		let match = value.match(/^(\w{6})(\w{6})(\w{6})(\w{6})$/);
 		if (match) {
-			return '' + match[1] + '-' + match[2] + '-' + match[3] + '-' + match[4]
+			return '' + match[1] + '-' + match[2] + '-' + match[3] + '-' + match[4];
 		}
-		return value
-	}
+		return value;
+	};
 
-	value=formatPhoneNumber()
+	value = formatPhoneNumber();
 
 	return (
 		<ContainerDetail propsHeader={propsHeader} {...props}>
-			<div className="container-detail" >
-				<div className="container-detail__qr" >
+			<div className="container-detail">
+				<div className="container-detail__qr">
 					<QRCode renderAs="svg" value={match.params.id} size={256} />
 				</div>
-				<div className="container-detail__text" >
+				<div className="container-detail__text">
 					<InputText name="name" label="Institución" value={value} onChange={hanldeChange} />
 				</div>
 			</div>
