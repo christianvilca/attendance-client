5b0c23510a16433fff8a59f2e0a66a835deb831f
Christian Vilca <christian.vilcaq@gmail.com>
2021-03-04 14:34:49 -0500

    fix: change text format for qr code

--- src/components/Pages/InstitutionQR.jsx
+++ src/components/Pages/InstitutionQR.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import ContainerDetail from '../Templates/ContainerDetail.jsx';
+import InputText from '../Molecules/InputText.jsx';
 import QRCode  from 'qrcode.react'
 
 const InstitutionQR = ({match, ...props}) => {
+
+	const hanldeChange =()=>{}
+
 	const propsHeader = {
 		title: 'Codigo de Institución',
 		to1: 'goBack',
 		icon1: 'arrow-back',
 		class1: 'button-header'
 	};
+
+	let value = match.params.id
+
+	const formatPhoneNumber = () => {
+		let match = value.match(/^(\w{6})(\w{6})(\w{6})(\w{6})$/)
+		console.log(match)
+		if (match) {
+			return '' + match[1] + '-' + match[2] + '-' + match[3] + '-' + match[4]
+		}
+		return value
+	}
+
+	value=formatPhoneNumber()
 
 	return (
 		<ContainerDetail propsHeader={propsHeader} {...props}>
@@ -17,11 +34,9 @@ const InstitutionQR = ({match, ...props}) => {
 					<QRCode renderAs="svg" value={match.params.id} size={256} />
 				</div>
 				<div className="container-detail__text" >
-
+					<InputText name="name" label="Institución" value={value} onChange={hanldeChange} />
 				</div>
-
 			</div>
-
 		</ContainerDetail>
 	);
 };
